eb72c7c536bfd00d8473ea83d0b138b0fde82a73
Minuk Ma <minukma@beaver01.cs.ubc.ca>
2025-02-19 22:56:36 -0800

    Make sphinxs docs based on ding group github

--- package/docs/source/conf.py
+++ package/docs/source/conf.py
@@ -5,7 +5,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))  # Important for finding your package
 
 project = 'EpitopeGen'
-copyright = '2024, Minuk Ma'
+copyright = '2025, Minuk Ma'
 author = 'Minuk Ma'
 release = '0.1.0'
 
@@ -18,7 +18,7 @@ extensions = [
 ]
 
 # Important for GitHub Pages
-html_baseurl = 'https://regaler.github.io/EpitopeGen/'
+html_baseurl = 'https://ding-group.github.io/EpitopeGen/'
 
 html_theme = 'sphinx_rtd_theme'  # Read the Docs theme
 html_static_path = ['_static']
